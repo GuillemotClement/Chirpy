7a298ef72312e6edb999cfb53a288c810ebe063c
GuillemotClement <guillemot.clement@protonmail.com>
2025-04-28 08:34:07 +0200

    Feat: first route

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	// définition du port dans une variable
 	const port = "8080"
+	// définition du folder contenant les fichiers statiques
+	const filePathRoot = "."
 
 	// création du nouveau routeur
 	mux := http.NewServeMux()
@@ -19,6 +21,14 @@ func main() {
 		// définis le routeur qui reçoit les requêtes
 		Handler: mux,
 	}
+
+	// pour la route "/", on viens retourner le fichier index.html
+	// placer dans le root du projet
+	// on pointe sur le serveurMux pour ajouter un handler qui gère le /
+	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+
+	// affichage dans le terminal
 	log.Printf("Serveur is listen on port : %s\n", port)
+	// affiche et stop si une erreur est lancer
 	log.Fatal(srv.ListenAndServe())
 }
