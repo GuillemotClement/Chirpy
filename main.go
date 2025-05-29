202f44efc5cb51c540e25485fa13855896399456
GuillemotClement <guillemot.clement@protonmail.com>
2025-05-29 10:59:22 +0200

    ajout du dossier de fichier statique + une route

--- main.go
+++ main.go
@@ -9,15 +9,16 @@ func main() {
 	// creation du constante pour le port utiliser
 	const port = "8080"
 	// definis une constante pour le dossier contenant les fichiers statique
-	const filepathRoot = "."
+	const filepathRoot = "./app"
 
 	// creation du serveur
 	mux := http.NewServeMux()
 
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+
 	// ajout d'une nouvelle route
-	// on indique le folder root comme contenant le fichier html
-	// retourne automatiquement le fichier index.html
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	// on declare une fonction handler pour gerer la route
+	mux.HandleFunc("/healthz", handleHealthz)
 
 	// parametre du serveur avec la struct
 	srv := http.Server{
@@ -27,4 +28,13 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe()) // pas besoin de passer le handler et addr qui sont definis dans la struct de parametre
+}
+
+func handleHealthz(w http.ResponseWriter, r *http.Request) {
+	// definir le header
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	// definis le status code => ici 200
+	w.WriteHeader(http.StatusOK)
+	// ecriture du body
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
