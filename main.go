d7c82734e65852d04ae230b43220782e5d44fb24
GuillemotClement <guillemot.clement@protonmail.com>
2025-05-29 10:31:22 +0200

    ajout de fichier statique

--- main.go
+++ main.go
@@ -8,14 +8,23 @@ import (
 func main() {
 	// creation du constante pour le port utiliser
 	const port = "8080"
+	// definis une constante pour le dossier contenant les fichiers statique
+	const filepathRoot = "."
+
 	// creation du serveur
 	mux := http.NewServeMux()
+
+	// ajout d'une nouvelle route
+	// on indique le folder root comme contenant le fichier html
+	// retourne automatiquement le fichier index.html
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
 	// parametre du serveur avec la struct
 	srv := http.Server{
 		Handler: mux,        // definition du handler
 		Addr:    ":" + port, // on concatene avec le port pour definir l'adresse
 	}
 
-	log.Printf("Starting server on port %s", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe()) // pas besoin de passer le handler et addr qui sont definis dans la struct de parametre
 }
